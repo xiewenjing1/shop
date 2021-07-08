6836f2934727c54136fe9a67d8b9b621c2f31283
xiewenjing1 <1836335642@qq.com>
2021-07-08 13:39:58 +0800

    优化导航栏商品清单

---
+++ src/views/home/config/config.js
@@ -0,0 +1,153 @@
+export const homeList = [{
+  id: 0,
+  content: [{
+    id: 0,
+    name: '手机',
+    value: ''
+  }, {
+    id: 1,
+    name: '数码',
+    value: ''
+  }]
+},
+{
+  id: 1,
+  content: [{
+    id: 0,
+    name: '电脑',
+    value: ''
+  }, {
+    id: 1,
+    name: '办公配件',
+    value: ''
+  }]
+},
+{
+  id: 2,
+  content: [{
+    id: 0,
+    name: '电视',
+    value: ''
+  }, {
+    id: 1,
+    name: '空调',
+    value: ''
+  }, {
+    id: 2,
+    name: '冰箱',
+    value: ''
+  }, {
+    id: 3,
+    name: '洗衣机',
+    value: ''
+  }]
+},
+{
+  id: 3,
+  content: [{
+    id: 0,
+    name: '厨卫家电',
+    value: ''
+  }, {
+    id: 1,
+    name: '小家电',
+    value: ''
+  }]
+},
+{
+  id: 4,
+  content: [{
+    id: 0,
+    name: '家具',
+    value: ''
+  }, {
+    id: 1,
+    name: '家具',
+    value: ''
+  }, {
+    id: 2,
+    name: '家装',
+    value: ''
+  }]
+},
+{
+  id: 5,
+  content: [{
+    id: 0,
+    name: '厨卫家电',
+    value: ''
+  }, {
+    id: 1,
+    name: '小家电',
+    value: ''
+  }]
+},
+{
+  id: 6,
+  content: [{
+    id: 0,
+    name: '个护化妆',
+    value: ''
+  }, {
+    id: 1,
+    name: '清洁用品',
+    value: ''
+  }, {
+    id: 2,
+    name: '纸品',
+    value: ''
+  }]
+},
+{
+  id: 7,
+  content: [{
+    id: 0,
+    name: '母婴用品',
+    value: ''
+  }, {
+    id: 1,
+    name: '儿童玩具',
+    value: ''
+  }, {
+    id: 2,
+    name: '童装童鞋',
+    value: ''
+  }]
+},
+{
+  id: 8,
+  content: [{
+    id: 0,
+    name: '鞋靴',
+    value: ''
+  }, {
+    id: 1,
+    name: '箱包',
+    value: ''
+  }, {
+    id: 2,
+    name: '钟表',
+    value: ''
+  }, {
+    id: 3,
+    name: '珠宝',
+    value: ''
+  }]
+},
+{
+  id: 9,
+  content: [{
+    id: 0,
+    name: '图书',
+    value: ''
+  }, {
+    id: 1,
+    name: '音像',
+    value: ''
+  }, {
+    id: 2,
+    name: '电子书',
+    value: ''
+  }]
+}
+];
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,53 +2,16 @@
   <div>
     <div class="w">
       <ul class="keywords-list">
-        <li class="keywords-item">
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','手机')">手机</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','数码')">数码</a>
-        </li>
-        <li class="keywords-item">
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','电脑')">电脑</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','办公配件')">办公配件</a>
-        </li>
-        <li class="keywords-item">
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','电视')">电视</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','空调')">空调</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','冰箱')">冰箱</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','洗衣机')">洗衣机</a>
-        </li>
-        <li class="keywords-item">
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','厨卫家电')">厨卫家电</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','小家电')">小家电</a>
-        </li>
-        <li class="keywords-item">
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','家具')">家具</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','家具')">家具</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','家装')">家装</a>
-        </li>
-        <li class="keywords-item">
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','厨卫家电')">厨卫家电</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','小家电')">小家电</a>
-        </li>
-        <li class="keywords-item">
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','个护化妆')">个护化妆</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','清洁用品')">清洁用品</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','纸品')">纸品</a>
-        </li>
-        <li class="keywords-item">
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','母婴用品')">母婴用品</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','儿童玩具')">儿童玩具</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','童装童鞋')">童装童鞋</a>
-        </li>
-        <li class="keywords-item">
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','鞋靴')">鞋靴</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','箱包')">箱包</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','钟表')">钟表</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','珠宝')">珠宝</a>
-        </li>
-        <li class="keywords-item">
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','图书')">图书</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','音像')">音像</a>
-          <a class="link" target="_blank" href="javascript:void(0)" @click="goGoodsListPage('','电子书')">电子书</a>
+        <li class="keywords-item" v-for="item in homeList" :key="item.id">
+          <a
+            v-for="nodeContent in item.content"
+            :key="nodeContent.id"
+            class="link"
+            target="_blank"
+            href="javascript:void(0)"
+            @click="goGoodsListPage('', nodeContent.name)"
+            >{{ nodeContent.name }}</a
+          >
         </li>
       </ul>
       <div class="banner-con">
@@ -57,13 +20,30 @@
       </div>
     </div>
     <div class="w">
-      <div class="floor-wrap" v-for="(category, index1) in  categoryData" :key="category.categoryId">
-        <h1 class="floor-title">F{{index1 + 1}} {{category.categoryName}}</h1>
+      <div
+        class="floor-wrap"
+        v-for="(category, index1) in categoryData"
+        :key="category.categoryId"
+      >
+        <h1 class="floor-title">
+          F{{ index1 + 1 }} {{ category.categoryName }}
+        </h1>
         <ul class="floor-list">
-          <li class="floor-item" v-for="(item, index2) in  category.categoryList" :key="item.categoryId">
-            <a href="javascript:void(0)" @click="goGoodsListPage(item.categoryId)">
-              <span class="floor-text">{{item.categoryName}}</span>
-              <img class="floor-img" :src="getImage(index1, index2)" alt="双开门冰箱"/>
+          <li
+            class="floor-item"
+            v-for="(item, index2) in category.categoryList"
+            :key="item.categoryId"
+          >
+            <a
+              href="javascript:void(0)"
+              @click="goGoodsListPage(item.categoryId)"
+            >
+              <span class="floor-text">{{ item.categoryName }}</span>
+              <img
+                class="floor-img"
+                :src="getImage(index1, index2)"
+                alt="双开门冰箱"
+              />
             </a>
           </li>
         </ul>
@@ -72,54 +52,65 @@
   </div>
 </template>
 <script type="text/ecmascript-6">
-  import pcBanner from 'components/banner';
+import pcBanner from "components/banner";
+import { homeList } from "./config/config";
 
-  export default {
-    data() {
-      return {
-        categoryData: [{
-          categoryId: '',
-          categoryName: '',
-          categoryList: [{
-            categoryId: '',
-            categoryName: ''
-          }]
-        }]
-      };
+export default {
+  data() {
+    return {
+      categoryData: [
+        {
+          categoryId: "",
+          categoryName: "",
+          categoryList: [
+            {
+              categoryId: "",
+              categoryName: "",
+            },
+          ],
+        },
+      ],
+      homeList,
+    };
+  },
+  mounted() {
+    console.log(this.homeList, 11111);
+  },
+  activated() {
+    // this.queryCategoryData((res) => {
+    //   if (res.code === 200) {
+    //     this.categoryData = res.result;
+    //   } else {
+    //     alert('加载主页面失败');
+    //   }
+    // });
+  },
+  methods: {
+    getImage(index1, index2) {
+      return require("../../assets/image/floor/floor" +
+        (index1 + 1) +
+        "-" +
+        (index2 + 1) +
+        ".jpg");
     },
-    created() {
+    goGoodsListPage(categoryId, keyword) {
+      let param = {};
+      param.categoryId = categoryId;
+      param.keyword = keyword;
+      this.loadPage("goods-list", param);
     },
-    activated() {
-      this.queryCategoryData((res) => {
-        if (res.code === 200) {
-          this.categoryData = res.result;
-        } else {
-          alert('加载主页面失败');
-        }
+    queryCategoryData(resolve) {
+      this.ajax({
+        type: "GET",
+        url: `/uac/auth/category/getProductCategoryDtoByPid/0`,
+        success: resolve,
       });
     },
-    methods: {
-      getImage(index1, index2) {
-        return require('../../assets/image/floor/floor' + (index1 + 1) + '-' + (index2 + 1) + '.jpg');
-      },
-      goGoodsListPage(categoryId, keyword) {
-        let param = {};
-        param.categoryId = categoryId;
-        param.keyword = keyword;
-        this.loadPage('goods-list', param);
-      },
-      queryCategoryData(resolve) {
-        this.ajax({
-          type: 'GET',
-          url: `/uac/auth/category/getProductCategoryDtoByPid/0`,
-          success: resolve
-        });
-      }
-    },
-    components: {
-      pcBanner
-    }
-  };
+  },
+  components: {
+    pcBanner,
+  },
+};
 </script>
 <style rel="stylesheet/scss" lang="scss">
 </style>
